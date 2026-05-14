9d3efd0f8dfaeea4bb3cdec55f329b35ed4953ce
vincent <ikennaakujekwe6@gmail.com>
2026-05-14 01:04:20 +0100

    tested and confirmed property endpoints working

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const { register, login } = require('../services/authService');
 const registerUser = async (req, res, next) => {
     try {
         const { name, email, password, role, phone } = req.body;
-        const user = await register(name, email, password, role, phone);
+        const user =  await register(name, email, password, role, phone);
         res.status(201).json({
              message: 'User registered successfully',
              data: {
@@ -22,8 +22,18 @@ const registerUser = async (req, res, next) => {
     const loginUser = async (req, res, next) => {
         try {
             const { email, password } = req.body;
-            const user = await login(email, password);
-            res.status(200).json({message: "login successful"});
+            const { user, token } = await login(email, password);
+            res.status(200).json({message: "login successful",
+                data: {
+                    token,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        email: user.email,
+                        role: user.role,
+                    }
+                }
+            });
         } catch (error) {
             next(error);
         }};
